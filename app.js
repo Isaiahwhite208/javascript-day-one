c9bf47e3c8df6c54ccf098025107101443c525cc
Isaiah White <isaiahwhite208@gmail.com>
2018-07-31 09:28:33 -0600

    push

---
+++ app.js
@@ -0,0 +1,318 @@
+let myLaptop = {
+    baseInterior: {
+        keyboard [
+            key: {
+                name: 'esc'
+            },
+            key: {
+                name: 'f1'
+            },
+            key: {
+                name: 'f2'
+            },
+            key: {
+                name: 'f3'
+            },
+            key: {
+                name: 'f4'
+            },
+            key: {
+                name: 'f5'
+            },
+            key: {
+                name: 'f6'
+            },
+            key: {
+                name: 'f7'
+            },
+            key: {
+                name: 'f8'
+            },
+            key: {
+                name: 'f9'
+            },
+            key: {
+                name: 'f10'
+            },
+            key: {
+                name: 'f11'
+            },
+            key: {
+                name: 'f12'
+            },
+            key: {
+                name: 'scorll (num lock)'
+            },
+            key: {
+                name: 'pause (break)'
+            },
+            key: {
+                name: 'insert (print screen)'
+            },
+            key: {
+                name: 'delete (system request)'
+            },
+            key: {
+                name: '`'
+            },
+            key: {
+                name: '1'
+            },
+            key: {
+                name: '2'
+            },
+            key: {
+                name: '3'
+            },
+            key: {
+                name: '4'
+            },
+            key: {
+                name: '5'
+            },
+            key: {
+                name: '6'
+            },
+            key: {
+                name: '7'
+            },
+            key: {
+                name: '8'
+            },
+            key: {
+                name: '9'
+            },
+            key: {
+                name: '0'
+            },
+            key: {
+                name: '-'
+            },
+            key: {
+                name: '='
+            },
+            key: {
+                name: 'backspace'
+            },
+            key: {
+                name: 'home'
+            },
+            key: {
+                name: 'tab'
+            },
+            key: {
+                name: 'q'
+            },
+            key: {
+                name: 'w'
+            },
+            key: {
+                name: 'e'
+            },
+            key: {
+                name: 'r'
+            },
+            key: {
+                name: 't'
+            },
+            key: {
+                name: 'y'
+            },
+            key: {
+                name: 'u'
+            },
+            key: {
+                name: 'i'
+            },
+            key: {
+                name: 'o'
+            },
+            key: {
+                name: 'p'
+            },
+            key: {
+                name: '['
+            },
+            key: {
+                name: ']'
+            },
+            key: {
+                name: '\'
+            },
+            key: {
+                name: 'page up'
+            },
+            key: {
+                name: 'caps lock'
+            },
+            key: {
+                name: 'a'
+            },
+            key: {
+                name: 's'
+            },
+            key: {
+                name: 'd'
+            },
+            key: {
+                name: 'f'
+            },
+            key: {
+                name: 'g'
+            },
+            key: {
+                name: 'h'
+            },
+            key: {
+                name: 'j'
+            },
+            key: {
+                name: 'k'
+            },
+            key: {
+                name: 'l'
+            },
+            key: {
+                name: ';'
+            },
+            key: {
+                name: ''
+            },
+            key: {
+                name: 'enter'
+            },
+            key: {
+                name: 'page down'
+            },
+            key: {
+                name: 'shift'
+            },
+            key: {
+                name: 'z'
+            },
+            key: {
+                name: 'x'
+            },
+            key: {
+                name: 'c'
+            },
+            key: {
+                name: 'v'
+            },
+            key: {
+                name: 'b'
+            },
+            key: {
+                name: 'n'
+            },
+            key: {
+                name: 'm'
+            },
+            key: {
+                name: ','
+            },
+            key: {
+                name: '.'
+            },
+            key: {
+                name: '/'
+            },
+            key: {
+                name: 'shift'
+            },
+            key: {
+                name: 'end'
+            },
+            key: {
+                name: 'ctrl'
+            },
+            key: {
+                name: 'function'
+            },
+            key: {
+                name: 'windows'
+            },
+            key: {
+                name: 'alt'
+            },
+            key: {
+                name: 'space bar'
+            },
+            key: {
+                name: 'alt'
+            },
+            key: {
+                name: 'tasks'
+            },
+            key: {
+                name: 'ctrl'
+            },
+            key: {
+                name: 'left arrow'
+            },
+            key: {
+                name: 'up arrow'
+            },
+            key: {
+                name: 'down arrow'
+            },
+            key: {
+                name: 'right arrow'
+            },
+        ],
+
+        baseExterior: [
+
+        ]
+    screenInterior: [
+
+        ]
+    screenExterior: [
+
+        ]
+    }
+}
+
+
+let myLaptop = {
+    baseInterior: [
+        keyboard: {
+            rowOne{
+                name: ["esc", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "scroll", "pause", "insert", "delete"],
+                color: "white",
+                backgroundColor: "black",
+                size: ""
+            },
+            rowTwo{
+                name: ["$`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace", "home"],
+                color: "white",
+                backgroundColor: "black",
+                size: 
+            },
+            rowThree{
+                name: ["tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "$[$", "$]$", "$\$", "page up"],
+                color: "white",
+                backgroundColor: "black",
+                size: 
+            },
+            rowFour{
+                name: ["caps lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", "$;$", "$'$", "enter", "page down"],
+                color: "white",
+                backgroundColor: "black",
+                size: 
+            },
+            rowFive{
+                name: ["shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "shift", "end"],
+                color: "white",
+                backgroundColor: "black",
+                size: 
+            },
+            rowSix{
+                name: ["ctrl", "function", "windows", "alt", "space bar", "alt", "tasks", "ctrl", "left arrow", "up arrow", "down arrow", "right arrow"],
+                color: "white",
+                backgroundColor: "black",
+                size: 
+            },
+        }
+    ]
+}
